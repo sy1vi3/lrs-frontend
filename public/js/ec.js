07cd2b7ee641bc1b4078805cae350e4962a65a47
Sylvie <cicadahunter99@gmail.com>
2023-10-09 06:35:06 -0400

    enter to send

--- public/js/ec.js
+++ public/js/ec.js
@@ -1011,3 +1011,11 @@ function fillStatsTable() {
     document.querySelector("#averageCombined").innerHTML = "Average Combined Score: " + avg_total;
 
 }
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
